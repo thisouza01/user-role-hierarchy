aed7e93774b1442297db2ed2741697647f56ddca
Thiago <tihsouza01@gmail.com>
2025-07-21 16:48:55 -0300

    Add: Testes para as classes.

--- user/Main.java
+++ user/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args){
+
+        Manager manager = new Manager("Thiago", "thiago@gmail.com", "123456789");
+        Salesman salesman = new Salesman("Thi", "thi@gmail.com", "123456789", 200);
+        Caxier caxier = new Caxier("ago", "ago@gmail.com", "123456789", 2000);
+
+        System.out.println(manager.getName());
+
+        System.out.println(salesman.getName());
+
+        System.out.println(caxier.getName());
+    }
+
 }
